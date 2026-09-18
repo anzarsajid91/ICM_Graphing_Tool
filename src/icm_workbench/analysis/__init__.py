2ca6dce823307240b061838741be8e755a065952
Anzar Sajid <anzarsajid@gmail.com>
2026-09-18 13:20:13 +0530

    Export validity-v1 analysis contract

--- src/icm_workbench/analysis/__init__.py
+++ src/icm_workbench/analysis/__init__.py
@@ -1,3 +1,4 @@
+from .validity import VALIDITY_STATES,CALCULATION_STATUSES,validity_summary,validity_status
 from .exclusions import apply_exclusions,audit_exclusions,exclusion_mask,normalise_exclusions
 from .integration import integrate_series,split_interval_by_month
 from .alignment import pair_series,interpolate_without_bridging,time_coverage
@@ -10,4 +11,13 @@ from .events import detect_rainfall_events
 from .diagnostics import residual_series,cumulative_volume,time_weighted_exceedance
 from .review import rating_curve_fit,weekly_data_assessment,dry_weather_flow,event_response_summary
 
-__all__=["apply_exclusions","audit_exclusions","exclusion_mask","normalise_exclusions","integrate_series","split_interval_by_month","pair_series","interpolate_without_bridging","time_coverage","calibration_metrics","detect_spill_intervals","apply_12_24_counting","monthly_spill_durations","spill_assessment","compare_scenarios","preview_time_offset","spill_block_volumes","idealised_storage_screening","rainfall_support_segments","rainfall_accumulation","daily_rainfall_support","detect_rainfall_events","residual_series","cumulative_volume","time_weighted_exceedance","rating_curve_fit","weekly_data_assessment","dry_weather_flow","event_response_summary"]
+__all__=[
+    "VALIDITY_STATES","CALCULATION_STATUSES","validity_summary","validity_status",
+    "apply_exclusions","audit_exclusions","exclusion_mask","normalise_exclusions",
+    "integrate_series","split_interval_by_month","pair_series","interpolate_without_bridging","time_coverage",
+    "calibration_metrics","detect_spill_intervals","apply_12_24_counting","monthly_spill_durations","spill_assessment",
+    "compare_scenarios","preview_time_offset","spill_block_volumes","idealised_storage_screening",
+    "rainfall_support_segments","rainfall_accumulation","daily_rainfall_support","detect_rainfall_events",
+    "residual_series","cumulative_volume","time_weighted_exceedance","rating_curve_fit","weekly_data_assessment",
+    "dry_weather_flow","event_response_summary",
+]
